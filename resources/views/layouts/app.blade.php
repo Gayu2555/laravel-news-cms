b24923701287c618ada8a7c74aa5f823a8d80bc6
Gayu Yunma Ramadhan <gayuyunma1123@gmail.com>
2025-03-11 15:55:21 +0700

    nambahin Google Tag Manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,20 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQH2L73V');</script>
+<!-- End Google Tag Manager -->
+
 </head>
 <body class="bg-gray-50 min-h-screen">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQH2L73V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!-- Sidebar -->
     <div class="fixed inset-y-0 left-0 w-64 bg-gray-900 text-white shadow-xl">
         <div class="p-6 border-b border-gray-800">
